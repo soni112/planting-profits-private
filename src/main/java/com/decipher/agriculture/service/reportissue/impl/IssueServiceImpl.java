7882b4e7b1959f75b5eacff5566347b1859986f1
chandrabhan <chandrabhan@decipherzone.com>
2017-09-12 12:54:16 +0530

    tasks #893 G121 Add Report an Issue feature is done

--- src/main/java/com/decipher/agriculture/service/reportissue/impl/IssueServiceImpl.java
+++ src/main/java/com/decipher/agriculture/service/reportissue/impl/IssueServiceImpl.java
@@ -3,6 +3,7 @@ package com.decipher.agriculture.service.reportissue.impl;
 import com.decipher.agriculture.dao.reportissue.IssueDao;
 import com.decipher.agriculture.data.reportissue.Issue;
 import com.decipher.agriculture.service.reportissue.IssueService;
+import com.decipher.util.email.SendEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +20,12 @@ public class IssueServiceImpl implements IssueService {
     @Autowired
     private IssueDao issueDao;
 
+    @Autowired
+    private SendEmail sendEmail;
+
     @Override
     public void createIssue(Issue issue) {
         issueDao.createIssue(issue);
+        sendEmail.sendEmail("issue@plantingprofit.com",issue.getEmail(),issue.getIssueRaised());
     }
 }
