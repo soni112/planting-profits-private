7baad2de86e93552357910c85a8149b9cb61a74d
Abhishek Samuel <abhisheks@decipherzone.com>
2018-01-13 16:06:54 +0530

    G353 : Reword crop limits sensitivity analysis

--- src/main/webapp/js/agriculture/output-farm-info.js
+++ src/main/webapp/js/agriculture/output-farm-info.js
@@ -607,7 +607,7 @@ function getStrategyForMultipleCrops() {
         customAlerts('The total of all Minimum crop acreage limits must be less than the total available land "' + maxLand + '"', type_error, time);
         return false;
     } else if (totalMaximumAcre > maxLand) {
-        customAlerts('Total of the Maximum acres amount must not be more than total available land "' + maxLand + '"', type_error, time);
+        customAlerts('Total of MAX acreage limits can not be more than total available acreage "' + maxLand + '"', type_error, time);
         return false;
     } else if (cropsArray.length == 0 && cropsGroupArray.length == 0 && cropContractArray.length == 0 && cropProposedArray.length == 0) {
         customAlerts("These are the original crop limits <br/> so a new strategy cannot be generated", 'error', time);
