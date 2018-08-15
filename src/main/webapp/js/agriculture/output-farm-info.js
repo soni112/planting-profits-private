ede2af0446b285a71ee7dccd3a2ae9d3e17e9149
ravisingh <ravi.kuntal6@gmail.com>
2018-08-15 18:02:49 +0530

    G590 : Change rewording bubble msg and fix comma and dollar sign

--- src/main/webapp/js/agriculture/output-farm-info.js
+++ src/main/webapp/js/agriculture/output-farm-info.js
@@ -235,7 +235,7 @@ function alterHTMLOfTableAndShowPopupTable(result) {
      * @desc - according to slide#2 of 02212016
      */
     /*$("#sensetiveAnalysisCropAndResourcePotentialProfitSpan").html("Estimated Income is "+potential_pro+".");*/
-    $("#sensetiveAnalysisCropAndResourcePotentialProfitSpan").html("Estimated Income : "+"&nbsp;&nbsp;&nbsp;" + "$"+CommaFormatted(potential_pro));
+    $("#sensetiveAnalysisCropAndResourcePotentialProfitSpan").html("Estimated Income : "+"&nbsp;&nbsp;&nbsp;" + "$"+potential_pro);
     if (unusedLand > 0) {
         $("#sensetiveAnalysisCropAndResourceUnusedSpan").html("&nbsp;&nbsp;&nbsp;" + unusedLand +" acres not assigned crops");
     }
@@ -354,14 +354,14 @@ function alterHTMLOfTableAndShowPopupTableForMultipalCropResourse(result) {
         }
     }
     var unusedLand = totalLand - usedLand;
-    var potential_pro = addCommaSignWithDollarForText(result.Potential_Profit);
+    var potential_pro = result.Potential_Profit;
     /**
      * @chanegd - Abhishek
      * @date - 25-02-2016
      * @desc - according to slide#2 of 02212016
      */
     /*$("#sensetiveAnalysisCropAndResourcePotentialProfitSpan").html("Estimated Income is "+potential_pro+".");*/
-    $("#sensetiveAnalysisCropAndResourcePotentialProfitSpan").html("Estimated Income : "+"&nbsp;&nbsp;&nbsp;" + "AHAHA");
+    $("#sensetiveAnalysisCropAndResourcePotentialProfitSpan").html("Estimated Income : "+"&nbsp;&nbsp;&nbsp;" + potential_pro);
     if (unusedLand > 0) {
         $("#sensetiveAnalysisCropAndResourceUnusedSpan").html("&nbsp;&nbsp;&nbsp;" + unusedLand +" acres not assigned crops");
     }
@@ -1930,28 +1930,4 @@ function checkMinMaxForCrop(currentRef) {
     } else {
         $("#max_min_selector").html("<option value=''>Select crop first</option>");
     }
-}
-
-function CommaFormatted(amount) {
-    var delimiter = ","; // replace comma if desired
-    var a = amount.split('.',2)
-    var d = a[1];
-    var i = parseInt(a[0]);
-    if(isNaN(i)) { return ''; }
-    var minus = '';
-    if(i < 0) { minus = '-'; }
-    i = Math.abs(i);
-    var n = new String(i);
-    var a = [];
-    while(n.length > 3) {
-        var nn = n.substr(n.length-3);
-        a.unshift(nn);
-        n = n.substr(0,n.length-3);
-    }
-    if(n.length > 0) { a.unshift(n); }
-    n = a.join(delimiter);
-    if(d.length < 1) { amount = n; }
-    else { amount = n + '.' + d; }
-    amount = minus + amount;
-    return amount;
 }
