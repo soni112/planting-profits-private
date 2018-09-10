4916bddf8fb860ada5545da23285a67ae5bcd042
soni <sonichahal@decipherzone.com>
2018-09-10 19:09:29 +0530

    Revert "G666: Changes in dashboard values."
    This reverts commit c24f64d5bce12f216e300b83eb805ee63d5d5702.

--- src/main/webapp/js/agriculture/output-farm-info.js
+++ src/main/webapp/js/agriculture/output-farm-info.js
@@ -528,28 +528,17 @@ function getStrategyForMultipleResources() {
 function validateResourceTableForSA() {
     var flag = true;
     $("#sa_multiple_resource_table tbody tr").each(function () {
-        var resourceName = $(this).children("td:nth(0)").text();
         var resourceOverridedValue = $(this).children("td:nth(2)").find("input").val().trim();
         if (resourceOverridedValue == "") {
             customAlerts("Please enter new amount for " + $(this).children("td:nth(0)").text().trim() + " resources", type_error, time);
             flag = false;
             return flag;
         }
-        else if (resourceName == "Land" || resourceName == "Working Capital") {
-            if (resourceOverridedValue == "0") {
+        else if (resourceOverridedValue == "0") {
             customAlerts("New resource amount can not be zero for " + $(this).children("td:nth(0)").text().trim() + " resources", type_error, time);
             flag = false;
             return flag;
-          }
         }
-        else if (resourceName !== "Land" || resourceName !== "Working Capital")
-        {
-            if (resourceOverridedValue == "0") {
-                flag = true;
-                return flag;
-           }
-        }
-
     });
     return flag;
 }
