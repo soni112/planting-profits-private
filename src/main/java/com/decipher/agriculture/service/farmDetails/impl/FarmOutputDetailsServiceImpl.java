0851222a24cf55c878ce135e86e520e60f65e898
ravi <ravi.singh@decipherzone.com>
2018-10-05 10:57:31 +0530

    G699: (shivam) Change in increase decrease for firm.

--- src/main/java/com/decipher/agriculture/service/farmDetails/impl/FarmOutputDetailsServiceImpl.java
+++ src/main/java/com/decipher/agriculture/service/farmDetails/impl/FarmOutputDetailsServiceImpl.java
@@ -165,23 +165,24 @@ public class FarmOutputDetailsServiceImpl implements FarmOutputDetailsService {
                         jsonObjectForFirm.put ( "cropName", cropTypeView.getCropName () + " (Firm)" );
 
                         /* checking Firm Condition for Increase Decrease w.r.t Profit Index */
+                        if (farmInfoView.getStrategy().equals(PlanByStrategy.PLAN_BY_ACRES)) {
+                            List<FarmOutputDetailsView> farmOutputDetailsViewList = (List<FarmOutputDetailsView>) outputDetails.get("farmOutputDetails");
+                            for (FarmOutputDetailsView farmOutputDetailsView : farmOutputDetailsViewList) {
+                                if (cropTypeView != null) {
+                                    if (cropTypeView.getFirmchecked().equalsIgnoreCase("true") && farmOutputDetailsView.getCropTypeView().getId().equals(cropTypeView.getId())) {
+                                        if (farmOutputDetailsView.getForFirm().equals(true)) {
+                                            double profitIndex = farmOutputDetailsView.getProfitIndex();
+                                            if (profitIndex >= 0.8) {
+                                                jsonObjectForFirm.put("incDecIncome", "Increase");
+                                            } else if (profitIndex < 0.8) {
+                                                jsonObjectForFirm.put("incDecIncome", "Decrease");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
 
-//                        List<FarmOutputDetailsView> farmOutputDetailsViewList = (List<FarmOutputDetailsView>) outputDetails.get("farmOutputDetails");
-//                            for (FarmOutputDetailsView farmOutputDetailsView : farmOutputDetailsViewList) {
-//                                if (cropTypeView != null) {
-//                                    if (cropTypeView.getFirmchecked().equalsIgnoreCase("true") && farmOutputDetailsView.getCropTypeView().getId().equals(cropTypeView.getId())) {
-//                                        if (farmOutputDetailsView.getForFirm() == true) {
-//                                            double profitIndex = farmOutputDetailsView.getProfitIndex();
-//                                            if (profitIndex >= 0.8) {
-//                                                jsonObjectForFirm.put("incDecIncome", "Increase");
-//                                            } else if (profitIndex < 0.8) {
-//                                                jsonObjectForFirm.put("incDecIncome", "Decrease");
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//
                         if (!jsonObjectForFirm.get ( IMPACTING_INCOME ).toString ().equalsIgnoreCase ( "--" )) {
                             jsonObjectForFirm.put ( "acreagePlanted", getCropAcreage ( cropTypeView, outputDetails, true ) );
                         } else if (jsonObjectForFirm.get ( IMPACTING_INCOME ).toString ().equalsIgnoreCase ( "--" )) {
